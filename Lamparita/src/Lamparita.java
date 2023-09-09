7fbf5ec930dcbd87f95a7ebbe7258e8a88d77788
Hernán <hpdelorenzo2014@gmail.com>
2023-09-08 21:02:36 -0300

    se modela una lamparita

--- Lamparita/src/Lamparita.java
+++ Lamparita/src/Lamparita.java
@@ -1,5 +1,36 @@
 public class Lamparita {
+
+    private boolean estado;
+
+    public Lamparita() {
+        this.estado = false;
+    }
+    public void encender(){
+        this.estado = true;
+    }
+    public void apagar(){
+        this.estado = false;
+    }
+    public boolean estaEncendida(){
+        return estado;
+    }
+
     public static void main(String[] args) throws Exception {
+        
         System.out.println("Vamos a programar una lamparita! ");
+
+        Lamparita lampara1 = new Lamparita();
+
+        System.out.println("La lámpara está encendida: " + lampara1.estaEncendida());
+
+        lampara1.encender();
+
+        System.out.println("La lámpara está encendida: " + lampara1.estaEncendida());
+
+        lampara1.apagar();
+
+        System.out.println("La lámpara está encendida: " + lampara1.estaEncendida());
     }
 }
+
+
